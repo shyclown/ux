42e46f1e32c8457b36abccf174d7fb94f34d6fdf
shyclown <roman.moravcik1@gmail.com>
2015-12-11 00:04:41 +0100

    just started

---
+++ js.js
@@ -0,0 +1,9 @@
+// UX Google material style
+
+var UEX = UEX || {};
+
+UEX.prototype.init = function(){
+  // find elements by specific class name
+  // attach event listeners to this elements
+  this.controledElements = document.getElementsByClassName('uex');
+}
